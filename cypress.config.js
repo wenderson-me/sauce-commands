f67d54d68b66dece5e094fb156dccd9958ee586d
Wenderson <49308695+wenderson-me@users.noreply.github.com>
2025-11-11 14:45:55 +0000

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
     baseUrl: 'https://www.saucedemo.com',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
